423751bc94b799d6d8bfcab0808988d7fb30dd6b
junior45263 <bc4363624@gmail.com>
2022-11-29 09:41:14 -0600

    v2

--- contacto.php
+++ contacto.php
@@ -8,5 +8,6 @@
 </head>
 <body>
     <h1>Soy el contacto</h1>
+    <h1>Soy el contacto2</h1>
 </body>
 </html>
--- index.php
+++ index.php
@@ -8,5 +8,6 @@
 </head>
 <body>
     <h1>Soy el index</h1>
+    <h2>Soy otra linea ok</h2>
 </body>
 </html>
